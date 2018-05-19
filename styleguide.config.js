515c8eb496aa82b90686daec7764cef83940dd25
Kelly Dwan <ryelle@users.noreply.github.com>
2018-05-18 20:50:11 -0400

    Try react styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,11 @@
+/** @format */
+
+module.exports = {
+	components: 'client/components/card/*.{js,jsx}',
+	ignore: [
+		// 'client/components/higher-order/**/*.{js,jsx}',
+		'**/test/**',
+		'**/*.test.{js,jsx}',
+		'**/*.spec.{js,jsx}',
+	],
+};
