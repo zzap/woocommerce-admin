e835acc11b4df1493355fa39a0d56b298f80b1aa
Justin Shreve <justin.shreve@gmail.com>
2019-01-17 09:08:59 -0500

    Hook up performance indicators to the REST API

--- client/lib/number/index.js
+++ client/lib/number/index.js
@@ -1,3 +1,11 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { formatCurrency } from '@woocommerce/currency';
+import { isFinite } from 'lodash';
+
 /**
  * Formats a number using site's current locale
  *
@@ -17,4 +25,31 @@ export function numberFormat( number ) {
 		return '';
 	}
 	return new Intl.NumberFormat( locale ).format( number );
+}
+
+export function formatValue( type, value ) {
+	if ( ! isFinite( value ) ) {
+		return null;
+	}
+
+	switch ( type ) {
+		case 'average':
+			return Math.round( value );
+		case 'currency':
+			return formatCurrency( value );
+		case 'number':
+			return numberFormat( value );
+	}
+}
+
+export function calculateDelta( primaryValue, secondaryValue ) {
+	if ( ! isFinite( primaryValue ) || ! isFinite( secondaryValue ) ) {
+		return null;
+	}
+
+	if ( secondaryValue === 0 ) {
+		return 0;
+	}
+
+	return Math.round( ( primaryValue - secondaryValue ) / secondaryValue * 100 );
 }
