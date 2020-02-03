679ba46186b7414c132626569ea77a17fe2a0d72
Paul Sealock <psealock@gmail.com>
2020-02-04 09:57:15 +1300

    Settings: reset source to global

--- client/settings/index.js
+++ client/settings/index.js
@@ -1,32 +1,62 @@
+/** @format */
+
+const SOURCE = typeof wcSettings === 'object' ? wcSettings : {};
+
+export const ADMIN_URL = SOURCE.adminUrl;
+export const COUNTRIES = SOURCE.countries;
+export const CURRENCY = SOURCE.currency;
+export const LOCALE = SOURCE.locale;
+export const ORDER_STATUSES = SOURCE.orderStatuses;
+export const SITE_TITLE = SOURCE.siteTitle;
+export const WC_ASSET_URL = SOURCE.wcAssetUrl;
+
 /**
- * External dependencies
+ * Retrieves a setting value from the setting state.
  *
- * @format
+ * @export
+ * @param {string}   name                         The identifier for the setting.
+ * @param {mixed}    [fallback=false]             The value to use as a fallback
+ *                                                if the setting is not in the
+ *                                                state.
+ * @param {function} [filter=( val ) => val]  	  A callback for filtering the
+ *                                                value before it's returned.
+ *                                                Receives both the found value
+ *                                                (if it exists for the key) and
+ *                                                the provided fallback arg.
+ *
+ * @returns {mixed}  The value present in the settings state for the given
+ *                   name.
  */
-
-import * as SHARED from '@woocommerce/settings';
+export function getSetting( name, fallback = false, filter = val => val ) {
+	const value = SOURCE.hasOwnProperty( name ) ? SOURCE[ name ] : fallback;
+	return filter( value, fallback );
+}
 
 /**
- * Internal dependencies
+ * Sets a value to a property on the settings state.
+ *
+ * @deprecated
+ *
+ * @export
+ * @param {string}   name                        The setting property key for the
+ *                                               setting being mutated.
+ * @param {mixed}    value                       The value to set.
+ * @param {function} [filter=( val ) => val]     Allows for providing a callback
+ *                                               to sanitize the setting (eg.
+ *                                               ensure it's a number)
  */
-import * as FALLBACKS from './fallbacks';
+export function setSetting( name, value, filter = val => val ) {
+	if ( [ 'wcAdminSettigs', 'onboarding' ].includes( name ) ) {
+		SOURCE[ name ] = filter( value );
+	}
+}
 
-// If `getSetting` is not set, then it was not available so let's do
-// defaults.
-const SOURCE = ! SHARED || typeof SHARED.getSetting === 'undefined' ? FALLBACKS : SHARED;
-
-export const ADMIN_URL = SOURCE.ADMIN_URL;
-export const COUNTRIES = SOURCE.COUNTRIES;
-export const CURRENCY = SOURCE.CURRENCY;
-export const LOCALE = SOURCE.LOCALE;
-export const ORDER_STATUSES = SOURCE.ORDER_STATUSES;
-export const SITE_TITLE = SOURCE.SITE_TITLE;
-export const WC_ASSET_URL = SOURCE.WC_ASSET_URL;
-export const DEFAULT_DATE_RANGE = SOURCE.DEFAULT_DATE_RANGE;
-
-export const getSetting = SOURCE.getSetting;
-export const setSetting = SOURCE.setSetting;
-
-// this needs to be shimmed separately because WooCommerce Blocks plugin version
-// might have `SHARED.getSetting` and thus `fallbacks.js` will not get loaded.
-export const getAdminLink = SOURCE.getAdminLink || FALLBACKS.getAdminLink;
+/**
+ * Returns a string with the site's wp-admin URL appended. JS version of `admin_url`.
+ *
+ * @param {String} path Relative path.
+ * @return {String} Full admin URL.
+ */
+export function getAdminLink( path ) {
+	return ( ADMIN_URL || '' ) + path;
+}
