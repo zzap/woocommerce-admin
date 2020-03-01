1ae8a8801283be8570c59d8cd2f3b791c608b83b
Rebecca Scott <me@becdetat.com>
2020-03-02 09:45:21 +1000

    Remove LinkButton in favour of the existing Button in @wordpress/components

--- client/dashboard/task-list/index.js
+++ client/dashboard/task-list/index.js
@@ -12,7 +12,7 @@ import { withDispatch } from '@wordpress/data';
 /**
  * WooCommerce dependencies
  */
-import { Card, List, MenuItem, EllipsisMenu, LinkButton } from '@woocommerce/components';
+import { Card, List, MenuItem, EllipsisMenu } from '@woocommerce/components';
 import { updateQueryString } from '@woocommerce/navigation';
 
 /**
@@ -258,13 +258,13 @@ class TaskDashboard extends Component {
 
 	onSkipStoreSetup = () => {
 		const completedTasksNames = this.getTasks()
-			.filter( x => x.completed )
-			.map( x => x.title );
+			.filter( ( x ) => x.completed )
+			.map( ( x ) => x.title );
 
 		recordEvent( 'wcadmin_tasklist_skip', {
 			completed_tasks: completedTasksNames.length,
 			completed_tasks_names: completedTasksNames,
-			skip_option: 'skip store setup'
+			skip_option: 'skip store setup',
 		} );
 
 		this.props.updateOptions( {
@@ -274,13 +274,13 @@ class TaskDashboard extends Component {
 
 	onDoThisLater = () => {
 		const completedTasksNames = this.getTasks()
-			.filter( x => x.completed )
-			.map( x => x.title );
+			.filter( ( x ) => x.completed )
+			.map( ( x ) => x.title );
 
 		recordEvent( 'wcadmin_tasklist_skip', {
 			completed_tasks: completedTasksNames.length,
 			completed_tasks_names: completedTasksNames,
-			skip_option: 'i\'ll do this later'
+			skip_option: "i'll do this later",
 		} );
 
 		this.props.updateOptions( {
@@ -294,15 +294,15 @@ class TaskDashboard extends Component {
 		return (
 			<div style={ { textAlign: 'center' } }>
 				{ ! skipStoreSetup && (
-					<LinkButton onClick={ this.onSkipStoreSetup }>
+					<Button isLink={ true } onClick={ this.onSkipStoreSetup }>
 						{ __( 'Skip store setup', 'woocommerce-admin' ) }
-					</LinkButton>				
+					</Button>
 				) }
 				{ ! skipStoreSetup && ! doThisLater && ' | ' }
 				{ ! doThisLater && (
-					<LinkButton onClick={ this.onDoThisLater }>
-						{ __( 'I\'ll do this later', 'woocommerce-admin' ) }
-					</LinkButton>
+					<Button isLink={ true } onClick={ this.onDoThisLater }>
+						{ __( "I'll do this later", 'woocommerce-admin' ) }
+					</Button>
 				) }
 			</div>
 		);
@@ -392,9 +392,19 @@ export default compose(
 			[ 'woocommerce_task_list_welcome_modal_dismissed' ],
 			false
 		);
-		const taskListPayments = getOptions( [ 'woocommerce_task_list_payments' ] );
-		const skipStoreSetup = get( options, [ 'woocommerce_task_list_skip_store_setup' ], false );
-		const doThisLater = get( options, [ 'woocommerce_task_list_do_this_later' ], false );
+		const taskListPayments = getOptions( [
+			'woocommerce_task_list_payments',
+		] );
+		const skipStoreSetup = get(
+			options,
+			[ 'woocommerce_task_list_skip_store_setup' ],
+			false
+		);
+		const doThisLater = get(
+			options,
+			[ 'woocommerce_task_list_do_this_later' ],
+			false
+		);
 
 		return {
 			modalDismissed,
