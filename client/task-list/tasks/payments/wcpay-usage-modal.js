0695aedb3b0776b0e2da73bb7d362455857c73d2
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-11-02 12:28:32 +0300

    Refactor WCPayUsageModal to work on top of UsageModal

--- client/task-list/tasks/payments/wcpay-usage-modal.js
+++ client/task-list/tasks/payments/wcpay-usage-modal.js
@@ -2,169 +2,60 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
-import { compose } from '@wordpress/compose';
-import { withDispatch, withSelect } from '@wordpress/data';
+import { useState } from '@wordpress/element';
 import { getQuery, updateQueryString } from '@woocommerce/navigation';
 import interpolateComponents from 'interpolate-components';
-import { Button, Modal } from '@wordpress/components';
 import { Link } from '@woocommerce/components';
-import { OPTIONS_STORE_NAME } from '@woocommerce/data';
 
-class WCPayUsageModal extends Component {
-	constructor( props ) {
-		super( props );
-		const query = getQuery();
-		this.state = {
-			isOpen:
-				! props.allowTracking &&
-				query[ 'wcpay-connection-success' ] === '1',
-			isLoadingScripts: false,
-			isRequestStarted: false,
-		};
+/**
+ * Internal dependencies
+ */
+import UsageModal from '../../../profile-wizard/steps/usage-modal';
+
+const WCPayUsageModal = () => {
+	const query = getQuery();
+	const shouldDisplayModal = query[ 'wcpay-connection-success' ] === '1';
+	const [ isOpen, setIsOpen ] = useState( shouldDisplayModal );
+
+	if ( ! isOpen ) {
+		return null;
 	}
 
-	async componentDidUpdate( prevProps, prevState ) {
-		const { hasErrors, isRequesting, createNotice } = this.props;
-		const { isLoadingScripts, isRequestStarted } = this.state;
-
-		// We can't rely on isRequesting props only because option update might be triggered by other component.
-		if ( ! isRequestStarted ) {
-			return;
-		}
-
-		const isRequestSuccessful =
-			! isRequesting &&
-			! isLoadingScripts &&
-			( prevProps.isRequesting || prevState.isLoadingScripts ) &&
-			! hasErrors;
-		const isRequestError =
-			! isRequesting && prevProps.isRequesting && hasErrors;
-
-		if ( isRequestSuccessful ) {
-			this.closeModal();
-			this.setState( { isRequestStarted: false } );
-		}
-
-		if ( isRequestError ) {
-			createNotice(
-				'error',
-				__(
-					'There was a problem updating your preferences.',
-					'woocommerce-admin'
-				)
-			);
-			this.closeModal();
-		}
-	}
-
-	enableTracking() {
-		const { updateOptions } = this.props;
-
-		if ( typeof window.wcTracks.enable === 'function' ) {
-			this.setState( { isLoadingScripts: true } );
-			window.wcTracks.enable( () => {
-				this.setState( { isLoadingScripts: false } );
-			} );
-		}
-
-		this.setState( { isRequestStarted: true } );
-		updateOptions( { woocommerce_allow_tracking: 'yes' } );
-	}
-
-	closeModal() {
-		this.setState( { isOpen: false, isRequestStarted: false } );
+	const closeModal = () => {
+		setIsOpen( false );
 		updateQueryString( { 'wcpay-connection-success': undefined } );
-	}
+	};
 
-	render() {
-		const { isOpen, isRequestStarted } = this.state;
-		const { isRequesting } = this.props;
-
-		if ( ! isOpen ) {
-			return null;
-		}
-
-		const isBusy = isRequestStarted && isRequesting;
-		const title = __(
-			'Help us build a better WooCommerce Payments experience',
+	const title = __(
+		'Help us build a better WooCommerce Payments experience',
+		'woocommerce-admin'
+	);
+	const trackingMessage = interpolateComponents( {
+		mixedString: __(
+			'By agreeing to share non-sensitive {{link}}usage data{{/link}}, you’ll help us improve features and optimize the WooCommerce Payments experience. You can opt out at any time.',
 			'woocommerce-admin'
-		);
-		const trackingMessage = interpolateComponents( {
-			mixedString: __(
-				'By agreeing to share non-sensitive {{link}}usage data{{/link}}, you’ll help us improve features and optimize the WooCommerce Payments experience. You can opt out at any time.',
-				'woocommerce-admin'
+		),
+		components: {
+			link: (
+				<Link
+					href="https://woocommerce.com/usage-tracking"
+					target="_blank"
+					type="external"
+				/>
 			),
-			components: {
-				link: (
-					<Link
-						href="https://woocommerce.com/usage-tracking"
-						target="_blank"
-						type="external"
-					/>
-				),
-			},
-		} );
+		},
+	} );
 
-		return (
-			<Modal
-				title={ title }
-				isDismissible={ false }
-				onRequestClose={ () => this.closeModal() }
-				className="woocommerce-payments__usage-modal"
-			>
-				<div className="woocommerce-payments__usage-wrapper">
-					<div className="woocommerce-payments__usage-modal-message">
-						{ trackingMessage }
-					</div>
-					<div className="woocommerce-payments__usage-footer">
-						<Button
-							isBusy={ isBusy }
-							onClick={ () => this.closeModal() }
-						>
-							{ __( 'No thanks', 'woocommerce-admin' ) }
-						</Button>
+	return (
+		<UsageModal
+			title={ title }
+			message={ trackingMessage }
+			acceptActionText={ __( 'I agree', 'woocommerce-admin' ) }
+			dismissActionText={ __( 'No thanks', 'woocommerce-admin' ) }
+			onContinue={ closeModal }
+			onClose={ closeModal }
+		/>
+	);
+};
 
-						<Button
-							isPrimary
-							isBusy={ isBusy }
-							onClick={ () => this.enableTracking() }
-						>
-							{ __( 'I agree', 'woocommerce-admin' ) }
-						</Button>
-					</div>
-				</div>
-			</Modal>
-		);
-	}
-}
-
-export default compose(
-	withSelect( ( select ) => {
-		const {
-			getOption,
-			getOptionsUpdatingError,
-			isOptionsUpdating,
-		} = select( OPTIONS_STORE_NAME );
-
-		const allowTracking =
-			getOption( 'woocommerce_allow_tracking' ) === 'yes';
-		const isRequesting = Boolean( isOptionsUpdating() );
-		const hasErrors = Boolean( getOptionsUpdatingError() );
-
-		return {
-			allowTracking,
-			isRequesting,
-			hasErrors,
-		};
-	} ),
-	withDispatch( ( dispatch ) => {
-		const { createNotice } = dispatch( 'core/notices' );
-		const { updateOptions } = dispatch( OPTIONS_STORE_NAME );
-
-		return {
-			createNotice,
-			updateOptions,
-		};
-	} )
-)( WCPayUsageModal );
+export default WCPayUsageModal;
